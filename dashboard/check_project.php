f5689864bc6d8410a546264c9332f54400bae2bc
Yohann Andre <yohann@mbp-de-yohann.home>
2024-04-19 20:51:14 +0200

    add cloudinary upload and started dotenv

--- dashboard/check_project.php
+++ dashboard/check_project.php
@@ -1,4 +1,46 @@
-<?php
+    <?php
+    include "includes/functions.php";
+    $error = false;
 
-var_dump($_POST);
-die();
+    if ($_FILES["project_img"]["error"] !== UPLOAD_ERR_OK) {
+        $error = "An_error_occured_while_uploading_image";
+        header("location: create_project.php?error=$error");
+    }
+
+    use Cloudinary\Api\Upload\UploadApi;
+
+    $image_uid = uniqid("img");
+
+    try {
+        // Chemin temporaire du fichier uploadé
+        $tmpFilePath = $_FILES["project_img"]["tmp_name"];
+        $upload = new UploadApi();
+
+        // Options
+        $options = [
+            'public_id' => $image_uid,
+            'use_filename' => true,
+            'overwrite' => false,
+            'allowed_formats' => ['jpg', 'jpeg', 'png'],
+            'folder' => "pixeven/project_img"
+        ];
+
+
+        $result = $upload->upload($tmpFilePath, $options);
+
+        var_dump($result);
+        die();
+
+        echo "<pre/>";
+        echo (json_encode($result, JSON_PRETTY_PRINT));
+        echo "<pre/>";
+    } catch (\Throwable $e) {
+        var_dump($e);
+        die();
+        header("location: create_project.php?error=$error");
+    }
+
+
+
+
+    ?>
--- dashboard/includes/config.php
+++ dashboard/includes/config.php
@@ -1,13 +1,49 @@
     <?php
-    // DB connexion
+    require_once __DIR__ . '../../../vendor/autoload.php';
+
+    $error_db = "error_no_results_found";
+
+    // **Dotenv config**
+
+    // Chemin vers le fichier .env
+    $dotenvPath = __DIR__ . '/../../.env';
+
+    // Charger les variables d'environnement depuis le fichier .env
+    $dotenv = Dotenv\Dotenv::createImmutable(dirname($dotenvPath));
+    $loaded = $dotenv->load();
+
+    // var_dump($loaded);
+
+    $cloudinary_api_key = getenv('CLOUDINARY_API_KEY');
+    $cloudinary_api_secret = getenv('CLOUDINARY_API_SECRET');
+    $cloudinary_cloud_name = getenv('CLOUDINARY_CLOUD_NAME');
+
+    var_dump(getenv('CLOUDINARY_API_KEY'));
+    die();
+
+
+    // **Dotenv config**
+
+
+    // **DB connexion** //
+
     $user = 'root';
     $pass = 'y';
     $host = "localhost";
     try {
-        $db = new PDO("mysql:host=$host;dbname=tp_5", $user, $pass);
+        $db = new PDO("mysql:host=$host;dbname=pixeven", $user, $pass);
     } catch (PDOException $e) {
         die("Error connexion to DB");
         // e->getmessage()
     }
 
-    $error_db = "error_no_results_found";
+    // **DB connexion** //
+
+    // **Cloudinary config**
+
+    // Use the Configuration class 
+    use Cloudinary\Configuration\Configuration;
+    // Configure an instance of your Cloudinary cloud
+    Configuration::instance('cloudinary://397588225888239:e_Y8joQRZWZVUiV3sXNiaLQZ_Pw@dwkwlok28?secure=true');
+
+    // **Cloudinary config**
